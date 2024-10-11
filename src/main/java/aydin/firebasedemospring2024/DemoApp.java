f70a9e3abd9b607b78f5c8ba0125d99f18710f4e
andrewkozinski <andrewkozinski76@gmail.com>
2024-10-11 16:06:09 -0400

    Added firebase code

--- src/main/java/aydin/firebasedemospring2024/DemoApp.java
+++ src/main/java/aydin/firebasedemospring2024/DemoApp.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import com.google.auth.oauth2.GoogleCredentials;
@@ -35,6 +36,17 @@ public class DemoApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
+        //Firebase code from website
+        FileInputStream serviceAccount = new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        //End firebase code
+
         fstore = contxtFirebase.firebase();
         fauth = FirebaseAuth.getInstance();
 
